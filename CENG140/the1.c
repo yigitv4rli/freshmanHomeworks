e4af82c731549cbe5ddd73f771f1679a7e0495ea
Yiğit Varlı <44034966+yigitv4rli@users.noreply.github.com>
2020-03-18 19:29:56 +0300

    Update the1.c

--- CENG140/the1.c
+++ CENG140/the1.c
@@ -4,13 +4,25 @@
 
 char curr_variable;
 int curr_start, curr_end, curr_step;
-char variables[52];
-int start_values[52], end_values[52], step_sizes[52], curr_values[52];
+char variables[255];
+int start_values[255], end_values[255], step_sizes[255], curr_values[255];
 int curr_loop = 0;
 
-int loop_variable_value(char c);
-void loop_continue(char c);
 
+void loop_execute(void) {
+    printf("X:%d r:%d Y:%d\n",
+        loop_variable_value('X'),
+        loop_variable_value('r'),
+        loop_variable_value('Y')); 
+}
+
+int side_finder(int a) { // (WORKS)
+    if (start_values[a] > end_values[a]) {
+        return 0;
+    } else if (start_values[a] < end_values[a]) {
+        return 1;
+    }
+}
 
 // Using for loop to find index in variables, then curr_values[index] = loop variable value (WORKS)
 int loop_variable_value(char c) {
@@ -23,37 +35,47 @@ int loop_variable_value(char c) {
     }
 }
 
-// Pointer make sense since we are changing the values? 
-int next_value(char c) {
+// Pointer make sense since we are changing the values? Or can we use arrays as pointers since it include pointers
+void next_value(char c) {
     int index;
     
     if (variables[0] == c) {
-        if (curr_values[0] + step_sizes[0] > end_values[0]) {
-                return 'signal';
-        } else {
-            curr_values[0] += step_sizes[0];
-        }
+        curr_values[0] += step_sizes[0];
     }
 
     for (index = 1; index < curr_loop; index++) {
-        int i;
-
         if (variables[index] == c) {
-            if (curr_values[index] + step_sizes[index] > end_values[index]){
-                curr_values[index] = start_values[index];
-                curr_values[index -1] = next_value(variables[index-1]);
-
-            } else {
-                curr_values[index] += step_sizes[index];
+            if (side_finder(index) == 1) {
+                if (curr_values[index] + step_sizes[index] > end_values[index]) {
+                    curr_values[index] = start_values[index];
+                    next_value(variables[index-1]);
+                } else {
+                    curr_values[index] += step_sizes[index];
+                }
+            } else if (side_finder(index) == 0) {
+                if (curr_values[index] + step_sizes[index] < end_values[index]) {
+                    curr_values[index] = start_values[index];
+                    next_value(variables[index-1]);
+                } else {
+                    curr_values[index] += step_sizes[index];
+                }
             }
-            
         }
     }
-    return 
 }
 
 void loop_continue(char c) {
-    
+    int index;
+
+    for (index = 0; index < curr_loop; index++) {
+        if (variables[index] == c) {
+            int i;
+            for (i = index; i < curr_loop; i++) {
+                curr_values[index] = start_values[index];
+            }
+        }
+    }
+    next_value(c);
 }
 
 int check_conditions(int start, int end, int step) {
@@ -65,27 +87,22 @@ int check_conditions(int start, int end, int step) {
         return 0;
     }
 }
-
-
-
+// Main function starts
 int main() {
-
-
-
     while (1) {
        int ch = getchar();
        if(ch == EOF) {
-          break;
+            break;
 
        } else {
-          ungetc(ch, stdin);
+            ungetc(ch, stdin);
        }
 
-       scanf(" %c", &curr_variable);
-       variables[curr_loop] = curr_variable;
+        scanf(" %c", &curr_variable);
+        variables[curr_loop] = curr_variable;
 
-       scanf(" %d %d %d", &curr_start, &curr_end, &curr_step);
-       if (check_conditions(curr_start, curr_end, curr_step) == 1) {
+        scanf(" %d %d %d", &curr_start, &curr_end, &curr_step);
+        if (check_conditions(curr_start, curr_end, curr_step) == 1) {
            goto finish;
 
        } else { 
@@ -97,10 +114,18 @@ int main() {
        }
     }
 
-
+    while (1) {
+        if (side_finder(0) == 1 && curr_values[0] > end_values[0]) {
+            break;
+        } else if (side_finder(0) == 0 && curr_values[0] < end_values[0]) {
+            break;
+        } else {
+            loop_execute();
+            next_value(variables[curr_loop-1]);
+        }
+    }
 
     finish:
-        printf("Something happened\n");
-
+        return 0;
     return 0;
 }
